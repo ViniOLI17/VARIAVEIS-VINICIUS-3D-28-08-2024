e9aa1db89c8c1559de3c2a3d700dd33c248e4123
ViniOLI17 <127845064+ViniOLI17@users.noreply.github.com>
2024-08-29 01:30:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let AnoAtual = 2024;
+
+const NomeCompleto = "Vinicius Oliveira";
+
+let Idade = 17;
+
+const CursoFaculdade = "x";
+
+let CidadeAtual = "São José dos pinhais";
+
+console.log(`Ola,meu nome é ${NomeCompleto},tenho ${Idade} anos,moro em ${CidadeAtual},quero cursar ${CursoFaculdade}`);
+
+AnoAtual = 2025;
+
+console.log(AnoAtual);
+
+console.log(`Ola,meu nome é ${NomeCompleto},o ano atual é ${AnoAtual},tenho ${Idade} anos,moro em ${CidadeAtual},quero cursar ${CursoFaculdade}`);
+
+AnoAtual = AnoAtual + 1;
+console.log(AnoAtual);
+
+Idade = Idade + 1;
+console.log(Idade);
